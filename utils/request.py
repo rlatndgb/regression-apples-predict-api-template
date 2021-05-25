2cbe7638e3b3d4ae71e8addfed34a03c9217888b
Sung Hyu Kim <kim.sh.luke@gmail.com>
2021-05-25 10:25:29 +0200

    API on AWS

--- utils/request.py
+++ utils/request.py
@@ -35,7 +35,7 @@ feature_vector_json = test.iloc[1].to_json()
 # NOTE: When testing your instance of the API on a remote machine
 # replace the URL below with its public IP:
 
-url = 'http://18.202.21.94:5000/api_v0.1'
+url = 'http://3.249.134.121:5000/api_v0.1'
 #url = 'http://127.0.0.1:5000/api_v0.1'
 
 # Perform the POST request.
